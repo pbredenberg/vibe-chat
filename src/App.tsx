224f89e17c5e008f97a99a3592915e3954763ee9
PaulBredenberg <paulbredenberg@gmail.com>
2025-07-09 20:39:40 -0400

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full">
+      <h1 className="text-3xl font-bold underline text-center">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
